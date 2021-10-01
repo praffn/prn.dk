8b3154018f314b3bc2510be99fa69bcf02de83a7
Phillip Raffnsøe <phillip@praffn.dk>
2021-10-01 14:06:51 +0200

    feat: update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,6 +28,29 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="favicon/favicon-16x16.png"
+          />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body className="bg-gray-50">
           <Main />

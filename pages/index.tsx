8ec9a45ea625d09e522dc7cb822371e7e5dd5530
Phillip Raffnsøe <phillip@praffn.dk>
2021-10-01 19:44:49 +0200

    fix: add missing alt text to 3d rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,10 @@ export default function Home({ posts }: HomeProps) {
           </div>
           <div className="mt-8 md:mt-0 flex-1 flex-shrink-0">
             <div className="max-w-xs  md:max-w-lg m-auto text-[0px]">
-              <Image src={prnClayImage} />
+              <Image
+                src={prnClayImage}
+                alt="Claylike cartoon 3D rendering of Phillip Raffnsøe Nilsson giving thumbs-up"
+              />
             </div>
           </div>
         </div>

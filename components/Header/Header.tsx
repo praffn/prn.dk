cc8e85feb65afdd64e2f361b229036b856d86c30
Phillip Raffnsøe <phillip@praffn.dk>
2021-10-02 15:55:16 +0200

    feat: add navigation to resume to header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,32 +4,18 @@ function Header() {
   return (
     <header className="py-4">
       <div className="container px-4 mx-auto py-2 flex items-center">
-        <div className="flex-1 flex justify-center">
-          <Link href="/">
-            <a className="mr-auto font-display text-3xl text-watermelon hover:underline">
-              prn
-            </a>
+        <Link href="/">
+          <a className="font-display text-3xl text-watermelon hover:underline">
+            prn
+          </a>
+        </Link>
+        <div className="ml-auto flex space-x-4">
+          <Link href="/resume">
+            <a className="button button-link">Résumé</a>
           </Link>
-        </div>
-
-        <nav className="flex-1 flex justify-center hidden">
-          <ul className="flex space-x-16">
-            <li>Services</li>
-            <li>
-              <Link href="/blog">
-                <a>Blog</a>
-              </Link>
-            </li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-
-        <div className="flex-1 flex justify-center">
-          {/* <Link href="/"> */}
-          <a href="mailto:phillip@praffn.dk" className="ml-auto cta">
+          <a href="mailto:phillip@praffn.dk" className="button button-primary">
             Get in touch
           </a>
-          {/* </Link> */}
         </div>
       </div>
     </header>

6f938fa462d3e60c36eb9a154f9d0ac54709ac05
Phillip Raffnsøe <phillip@praffn.dk>
2021-12-10 01:10:11 +0100

    chore: upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ const fontFamilyDisplay = [
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
   theme: {
     fontFamily: {
